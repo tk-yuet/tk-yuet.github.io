ba75010fd7540602f0e8e39ded467ac5cb6fa8cd
Yuet Ying Lau <irislau@Iriss-MBP-TW.local>
2021-05-30 19:47:33 +0100

    Updates

---
+++ _next/static/gNQYEBrvJef_RKoVmD5Sf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-d30f030f6801abf1b12f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8c798a59fc08a5db62a8.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
